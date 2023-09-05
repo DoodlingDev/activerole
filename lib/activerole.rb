968582c87fdd4b493c146e9aefb4c8cab0de7d2d
Aji Slater <aji.slater@gmail.com>
2023-09-04 20:29:34 -0500

    initial commit

---
+++ lib/activerole.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "activerole/version"
+
+module Activerole
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/activerole/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Activerole
+  VERSION = "0.1.0"
+end
